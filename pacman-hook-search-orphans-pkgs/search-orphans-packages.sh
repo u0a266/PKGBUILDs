119a15ed3fbfeab4b5e96ae54503f81785ce8705
u0a266 <72482919+u0a266@users.noreply.github.com>
2021-06-15 22:13:31 +0900

    Upd

--- pacman-hook-search-orphans-pkgs/search-orphans-packages.sh
+++ pacman-hook-search-orphans-pkgs/search-orphans-packages.sh
@@ -2,10 +2,9 @@
 all_off="$(tput sgr0)"
 bold="${all_off}$(tput bold)"
 blue="${bold}$(tput setaf 4)"
-yellow="${bold}$(tput setaf 3)"
 
 note() {
-    printf "${blue}::${yellow} pacman:${bold} $1${all_off}\n"
+    printf "${blue}:: ${bold}$1${all_off}\n"
 }
 
 pacman -Qdtt || note "孤立パッケージは見つかりませんでした" 
